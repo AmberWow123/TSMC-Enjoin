ad3a912ebf0b8fd8318d75644c44500f953718b2
dundunbug <yahsin12345@yahoo.com>
2021-07-25 18:19:16 +0800

    edit account.py, add cloud db

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
 from models import mongo
+import dns
+import json
 
+
+f = open('key.json', 'r')
+data = json.load(f)
+user = data["user"]
+password = data["password"]
+
+# url = "mongodb://localhost:27017/hackathon"
+url = "mongodb+srv://"+user+":"+password+"@cluster0.56zcf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/hackathon"
+app.config["MONGO_URI"] = url
 mongo.init_app(app)
+
 
 from routes import *
 app.register_blueprint(routes)
--- routes/account.py
+++ routes/account.py
@@ -1,5 +1,14 @@
-from flask import jsonify
+from flask import jsonify, request
 from . import routes, db
+from bson.objectid import ObjectId
+
+# {    # account
+#     id: 工號(int) # primary key, not null
+#     password: (string) # not null
+#     FAB: FAB2(選單) # not null
+#     ownList: []
+#     joinList: [1,2,3]
+# }
 
 #account api
 @routes.route('/test', methods=['GET'])
@@ -12,5 +21,31 @@ def testDB():
     print(db.collection_names())
     return jsonify(message='it works!')
 
+#create account
+@routes.route("/account/create", methods=['POST'])
+def add_one():
+    json = request.get_json()
+    print(json)
+    _id = json['id']
+    password = json['password']
+    fab = json['fab']
 
+    db['account'].insert_one({'id': _id, 'password': password, 'fab': fab})
+    return jsonify(message="success")
 
+#update joinlist
+@routes.route("/account/updatejoinList/<string:uuid>", methods=['POST'])
+def updateList(uuid):
+    print("uuid ", uuid)
+    json = request.get_json(force=True)
+    print(json)
+    lst = [json['list']]
+
+    result = db['account'].find_one({'_id': ObjectId(uuid)})
+    print(result)
+    if 'joinList' in result:
+        lst += result['joinList']
+    print(lst)
+    db['account'].update_one({'_id': ObjectId(uuid)}, {"$set": {'joinList': lst}})
+
+    return jsonify(message="success")
