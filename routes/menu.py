0601869fda2728f6bc3abe9abdad5a8d94180f2f
Yi Ting Wang <50982563+AmberWow123@users.noreply.github.com>
2021-07-27 03:35:32 -0700

    feat: finished show all factories

--- routes/menu.py
+++ routes/menu.py
@@ -1,5 +1,7 @@
 from flask import jsonify, request
 from . import routes, db
+import json
+from bson import json_util
 
 
 # factory table 
@@ -16,11 +18,19 @@ from . import routes, db
 
 @routes.route('/Menu/ListFactory/', methods=['GET'])
 def show_all_factories():
-    fab_table = db.factory
-    menu_table = db.menu
+    fab_table = db['factory']
+    menu_table = db['menu']
     
-    output = []
+    output = {}
 
     for fab in fab_table.find():
-        output.append( {'fab': fab['name'], 'stores': fab['stores']} )
-    return jsonify({'output': output})
+        store_menu = {}
+        for store in fab['stores']:
+            fd_dr = menu_table.find({'name': store}, { "_id": 0, "name": 0 })[0]
+            store_menu[store] = fd_dr
+
+        output[fab['name']] = store_menu
+    
+
+    result = json.loads(json_util.dumps(output))
+    return result
