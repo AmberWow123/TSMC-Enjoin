fd9d4c524d0a5b197dad1c5447a2cf3c3e068b3f
dundunbug <yahsin12345@yahoo.com>
2021-07-29 11:55:35 +0800

    edit account

--- routes/account.py
+++ routes/account.py
@@ -6,8 +6,8 @@ from bson.objectid import ObjectId
 #     id: 工號(int) # primary key, not null
 #     password: (string) # not null
 #     FAB: FAB2(選單) # not null
-#     ownList: []
-#     joinList: [1,2,3]
+#     ownOrder: []
+#     joinOrder: []
 # }
 
 #account api
@@ -22,30 +22,56 @@ def testDB():
     return jsonify(message='it works!')
 
 #create account
-@routes.route("/account/create", methods=['POST'])
-def add_one():
-    json = request.get_json()
-    print(json)
-    _id = json['id']
-    password = json['password']
-    fab = json['fab']
+@routes.route("/Account/Create", methods=['POST'])
+def accountCreate():
+    _id = request.form['id']
+    password = request.form['password']
+    fab = request.form['fab']
 
     db['account'].insert_one({'id': _id, 'password': password, 'fab': fab})
-    return jsonify(message="success")
+    
+    result = db['account'].find_one({'id': id})
+    if result:
+        response = jsonify(message="error")
+    else:
+        response = jsonify(message="success")
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers['Access-Control-Allow-Method'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
+@routes.route("Account/Login", methods=['POST'])
+def accountLogin():
+    _id = request.form['id']
+    password = request.form['password']
+    result = db['account'].find_one({'id': id})
+    if result:
+        if result['password'] == password:
+            response = jsonify(message="成功登入")
+        else:
+            response = jsonify(message="登入失敗")
+    else:
+        response = jsonify(message="查無此帳號")
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers['Access-Control-Allow-Method'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response    
+
+
 
 #update joinlist
-@routes.route("/account/updatejoinList/<string:uuid>", methods=['POST'])
-def updateList(uuid):
-    print("uuid ", uuid)
-    json = request.get_json(force=True)
-    print(json)
-    lst = [json['list']]
+# @routes.route("/Account/updatejoinList/<string:uuid>", methods=['POST'])
+# def updateList(uuid):
+#     print("uuid ", uuid)
+#     json = request.get_json(force=True)
+#     print(json)
+#     lst = [json['list']]
 
-    result = db['account'].find_one({'_id': ObjectId(uuid)})
-    print(result)
-    if 'joinList' in result:
-        lst += result['joinList']
-    print(lst)
-    db['account'].update_one({'_id': ObjectId(uuid)}, {"$set": {'joinList': lst}})
+#     result = db['account'].find_one({'_id': ObjectId(uuid)})
+#     print(result)
+#     if 'joinList' in result:
+#         lst += result['joinList']
+#     print(lst)
+#     db['account'].update_one({'_id': ObjectId(uuid)}, {"$set": {'joinList': lst}})
 
-    return jsonify(message="success")
+#     return jsonify(message="success")
