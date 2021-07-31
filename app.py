210f4d4ac097325a61b39f610540b4fd055bceb1
tujahhow <tujahhow@gmail.com>
2021-07-31 09:01:29 +0800

    auto encode ObjectId + 中文顯示

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from models import mongo
 import dns
 import json
 
+from flask.json import JSONEncoder
+class CustomJsonEncoder(JSONEncoder):
+    def default(self, o):
+        from bson.objectid import ObjectId
+        if isinstance(o, ObjectId):
+            return str(o)
+        return super().default(o)
 
 f = open('key.json', 'r')
 data = json.load(f)
@@ -13,6 +20,8 @@ db = data["db"]
 # url = "mongodb://localhost:27017/hackathon"
 url = "mongodb+srv://"+user+":"+password+db
 app = Flask(__name__)
+app.json_encoder=CustomJsonEncoder
+app.config['JSON_AS_ASCII'] = False
 CORS(app)
 app.config["MONGO_URI"] = url
 mongo.init_app(app)
