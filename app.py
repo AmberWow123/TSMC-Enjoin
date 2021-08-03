271ab4e65651326d93d5e073aca5855a53790215
家 浩 <tujahhow@gmail.com>
2021-08-03 05:49:28 +0000

    node.js like static server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, send_from_directory, render_template
+from flask import send_from_directory, redirect
 from flask_cors import CORS
 from models import mongo
 import dns
@@ -41,7 +42,20 @@ app.register_blueprint(routes)
 # def home():
 #     return render_template("index.html")
 
+@app.route('/', defaults={'path': 'index.html'}, methods=['GET'])
+@app.route('/<path:path>', methods=['GET'])
+def staticHost(path):
+    try:
+        return send_from_directory(app.static_folder, path)
+    except Exception:
+        if path[-1]=='/':
+            return send_from_directory(app.static_folder, path+'index.html')
+        else:
+            return redirect(request.url+'/')
+
 if __name__ == '__main__':
     app.debug = True
+    if app.debug:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run()
     
