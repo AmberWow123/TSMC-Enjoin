c4da0b1bad5240de2aedb149bcc8ff9430abdaff
Perry <ch.parry1233@gmail.com>
2021-08-04 01:56:05 +0000

    clear code/ remove hashtag input/ make input authentication

--- static/profile/js/admin.js
+++ static/profile/js/admin.js
@@ -2,10 +2,8 @@ import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 const apiUrl = "https://tsmc-enjoin.herokuapp.com";
 //const apiUrl = `http://127.0.0.1:5000`;
 
-let productModal = null;
 let orderModal = null;
 let closeOrderModal = null;
-let delProductModal = null;
 let delOrderModal = null;
 let followModal = null;
 //let token = null;
@@ -33,52 +31,13 @@ createApp({
   data() {
     return {
       followList : [],
-      defaultFollowList:[
-
-      ],
       ownList : [],
-      defaultList: [{
-        "_id": "6100c77a668482b3ac89b8c9",
-        "comment": "邊緣人急需人QQQ",
-        "creator_id": "2266555",
-        "drink": "美式咖啡",
-        "hashtag": [
-            "FAB7",
-            "starbucks",
-            "美式咖啡"
-        ],
-        "join_people": 2,
-        "join_people_bound": 2,
-        "join_people_id": [
-            "123456",
-            "167345"
-        ],
-        "meet_factory": "FAB7",
-        "meet_time": [
-            "2021-07-28T18:00:00.000Z",
-            "2021-07-28T18:15:00.000Z"
-        ],
-        "status": "COMPLETED",
-        "store": "starbucks",
-        "title": "美式咖啡買三送二"
-    }],
-      products: [],
       isNew: false,
-      tempProduct: {
-        imagesUrl: [],
-      },
       tempOrder: {
       },
     };
   },
   mounted() {
-    
-    productModal = new bootstrap.Modal(
-      document.getElementById("productModal"),
-      {
-        keyboard: false,
-      }
-    );
     orderModal = new bootstrap.Modal(
       document.getElementById("orderModal"),
       {
@@ -87,12 +46,6 @@ createApp({
     );
     closeOrderModal = new bootstrap.Modal(
       document.getElementById("closeOrderModal"),
-      {
-        keyboard: false,
-      }
-    );
-    delProductModal = new bootstrap.Modal(
-      document.getElementById("delProductModal"),
       {
         keyboard: false,
       }
@@ -180,46 +133,6 @@ createApp({
         .catch((err) => {
           console.log(err);
         });
-    },
-    
-    updateProduct() {
-      let url = `${apiUrl}/${apiPath}/admin/product`;
-      let http = "post";
-      if (!this.isNew) {
-        url = `${apiUrl}/${apiPath}/admin/product/${this.tempProduct.id}`;
-        http = "put";
-      }
-      axios[http](url, { data: this.tempProduct })
-        .then((res) => {
-          if (res.data.success) {
-            alert(res.data.message);
-            productModal.hide();
-            this.getData();
-          } else {
-            alert(res.data.message);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      
-      // if (this.isNew) {
-      //   //當產品不存在，新增至產品列表
-      //   this.products.push({
-      //     id: Date.now(),
-      //     ...this.tempProduct,
-      //   });
-      //   this.tempProduct = {
-      //     imagesUrl: [], //清空 tempProduct
-      //   };
-      //   productModal.hide();
-      // } else {
-      //   const index = this.products.findIndex(
-      //     (item) => item.id === this.tempProduct.id
-      //   );
-      //   this.products[index] = this.tempProduct;
-      //   productModal.hide();
-      // }
     },
 
     updateOrder() {
@@ -319,30 +232,6 @@ createApp({
           console.log(err);
         });
     },
-   
-    delProduct() {
-      axios
-        .delete(`${apiUrl}/${apiPath}/admin/product/${this.tempProduct.id}`)
-        .then((res) => {
-          if (res.data.success) {
-            alert(res.data.message);
-            delProductModal.hide();
-            this.getData();
-          } else {
-            console.log(res.data.message);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      
-      //   this.products.splice(
-      //     this.products.findIndex((item) => item.id === this.tempProduct.id),
-      //     1
-      //   );
-      //   delProductModal.hide();
-      // },
-    },
 
 
     openModal(status, item) {
@@ -390,12 +279,6 @@ createApp({
       else if (status === "getFoloow") {
         followModal.show();
       }
-    },
-    
-    
-    createImages() {
-      this.tempProduct.imagesUrl = [];
-      this.tempProduct.imagesUrl.push("");
     },
     
     logout() {
