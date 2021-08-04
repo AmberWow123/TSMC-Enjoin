b68bd66c11fa7e5290dcdd22e298535fd2593647
Perry <ch.parry1233@gmail.com>
2021-08-04 10:46:07 +0000

    edit on loginRegisterPage

--- static/LoginRegister/js/login.js
+++ static/LoginRegister/js/login.js
@@ -8,7 +8,11 @@ createApp({
       user: {
         id: "",
         password: "",
-        fab: ""
+      },
+      register: {
+        id: "",
+        password: "",
+        epidemic_prevention_group: ""
       },
     };
   },
@@ -39,65 +43,11 @@ createApp({
 
     create() {
       axios
-        .post(`${this.apiUrl}/Account/Create`, this.user)
+        .post(`${this.apiUrl}/Account/Create`, this.register)
         .then((res) => {
           console.log(res);
           alert(res.data.message);
           
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-
-    loginForm() {
-      const formData = new FormData()
-      formData.append('id', this.user.id)
-      formData.append('password', this.user.password)
-      formData.append('fab',this.user.fab)
-
-      axios
-        .post(`${this.apiUrl}/Account/Login`, formData)
-        .then((res) => {
-          console.log(res);
-          if (res.data.message==="成功登入") {
-            const { token, expired, _id, id } = res.data;
-            // Warn!
-            // console.log('expired:',expired) // gives 'undefined'
-            document.cookie = `Token=${token};expires=${new Date(
-              expired
-            )}; path=/`;
-            document.cookie = `_id=${_id}; path=/`
-            document.cookie = `id=${id}; path=/`
-            window.location.href = `../profile?id=${this.user.id}`;
-          } else {
-            alert(res.data.message);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-
-    createForm() {
-      const formData = new FormData()
-      formData.append('id', this.user.id)
-      formData.append('password', this.user.password)
-      formData.append('fab',this.user.fab)
-
-      axios
-        .post(`${this.apiUrl}/Account/Create`, formData)
-        .then((res) => {
-          console.log(res);
-          if (res.data.success) {
-            const { token, expired } = res.data;
-            document.cookie = `Token=${token};expires=${new Date(
-              expired
-            )}; path=/`;
-            //window.location = "group.html";
-          } else {
-            alert(res.data.message);
-          }
         })
         .catch((err) => {
           console.log(err);
