a0e1873ff2f73d468eb2d74f77d93f97b44e8e6b
Perry <ch.parry1233@gmail.com>
2021-08-05 08:02:12 +0000

    replace DEL with POST req

--- static/profile/js/admin.js
+++ static/profile/js/admin.js
@@ -276,7 +276,7 @@ createApp({
       let button = document.getElementById("leaveOrderBTN");
       button.disabled = true;
       axios
-        .delete(`${apiUrl}/Order/QuitOrder/${param}/${this.tempOrder._id}`,{
+        .post(`${apiUrl}/Order/QuitOrder/${param}/${this.tempOrder._id}`,{
           headers: {'x-access-token': token}
         })
         .then((res) => {
