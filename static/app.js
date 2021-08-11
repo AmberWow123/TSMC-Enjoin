895aeadc2e4842663ce030686c374bb5e24c27d0
YahsinTeng <yahsin12345@yahoo.com>
2021-08-11 01:11:53 +0000

    create branch

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://tsmc-enjoin.herokuapp.com'
+const apiUrl = 'https://enjoin-tsmc.herokuapp.com'
 const container = document.getElementById("app");
 
 function getCookie(name) {
